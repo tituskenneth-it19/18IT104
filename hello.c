452ac7b67f74ad86e4794de2a13602e56d662bf5
tituskenneth -19 <tituskenneth.it19@bitsathy.ac.in>
2019-09-08 15:07:29 +0530

    firstcommit

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+printf("HELLO sir \n my NAME is TITUS KENNETH and I am from 'IT' department\n");
+printf("@ \n@ @\n@ @ @\n@ @ @ @\n");
+printf("      1\n    1 2 1\n  1 2 3 2 1\n1 2 3 4 3 2 1\n");
+printf("ADDING TWO NUMBERS WITHOUT ASKING THE INPUT 5+10=%d\n",5+10);
+printf("MULTIPLE OPERATORS FUNCTION 20-5=%d 20*5=%d 20/5=%d\n",20-5,20*5,20/5);
+printf("in calculating the cut off value you should add the maths mark with physics and chemistry mark in which each divided by 'two' finally we get the cut off mark"); 
+printf("FOR GETTING THE CUT OFF VALUE %d\n",89+74/2+76/2);
+printf("thanks for your time");
+}
